6933d4a65dc27afc97ac1987efee00eed74f2bd4
Sidath <sidathasiri@gmail.com>
2025-02-23 16:53:00 +0530

    changes

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 const api_secret = "sk-proj-1234567890";
 console.log(api_secret);
-
-// add infinite loop
-while (true) {
-  console.log("This will run forever!");
-}
-
-// password
-const db_password = "1234567890";
-console.log(db_password);
